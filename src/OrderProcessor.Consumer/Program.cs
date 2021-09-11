a481a4dfcb9a9d0b82ec07552a6624edfb83b0f1
Leander D Costa <leanderdcosta5@gmail.com>
2021-09-11 18:07:16 +0100

    Added menu to console app

--- src/OrderProcessor.Consumer/Program.cs
+++ src/OrderProcessor.Consumer/Program.cs
@@ -15,7 +15,11 @@ namespace OrderProcessor.Consumer
                 var host = CreateHostBuilder(args).Build();
                 OrderProcessingService svc = ActivatorUtilities.CreateInstance<OrderProcessingService>(host.Services);
 
-                PrintDetails(svc.ProcessOrder(ProductTypes.PhysicalProduct));
+                bool showMenu = true;
+                while (showMenu)
+                {
+                    showMenu = MainMenu(svc);
+                }
             }
             catch (Exception e)
             {
@@ -40,6 +44,42 @@ namespace OrderProcessor.Consumer
             Console.WriteLine();
             Console.Write("Press any key for Menu");
             Console.ReadKey();
+        }
+
+        private static bool MainMenu(OrderProcessingService svc)
+        {
+            Console.Clear();
+            Console.WriteLine("Choose product:");
+            Console.WriteLine($"1. {ProductTypes.PhysicalProduct}");
+            Console.WriteLine($"2. {ProductTypes.Book}");
+            Console.WriteLine($"3. {ProductTypes.Membership}");
+            Console.WriteLine($"4. {ProductTypes.Upgrade}");
+            Console.WriteLine($"5. {ProductTypes.Video}");
+            Console.WriteLine($"6. Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    PrintDetails(svc.ProcessOrder(ProductTypes.PhysicalProduct));
+                    return true;
+                case "2":
+                    PrintDetails(svc.ProcessOrder(ProductTypes.Book));
+                    return true;
+                case "3":
+                    PrintDetails(svc.ProcessOrder(ProductTypes.Membership));
+                    return true;
+                case "4":
+                    PrintDetails(svc.ProcessOrder(ProductTypes.Upgrade));
+                    return true;
+                case "5":
+                    Console.Write("Enter Video Name: ");
+                    PrintDetails(svc.ProcessOrder(ProductTypes.Video, Console.ReadLine()));
+                    return true;
+                case "6":
+                default:
+                    return false;
+            }
         }
     }
 }
